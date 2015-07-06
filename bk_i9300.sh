2e0d0e89d78cfb097e502512cd095168e1ff4e80
1n4148 <azapfig@hotmail.de>
2015-07-06 21:52:49 +0200

    Added build scripts & installer

---
+++ bk_i9300.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+export KERNELDIR=`readlink -f .`
+
+make defconfig cyanogenmod_i9300_defconfig
+
+. $KERNELDIR/.config
+
+mv .git .git-halt
+
+cd $KERNELDIR/
+make -j4 || exit 1
+
+mkdir -p $KERNELDIR/BUILT_I9300/lib/modules
+
+rm $KERNELDIR/BUILT_I9300/lib/modules/*
+rm $KERNELDIR/BUILT_I9300/zImage
+
+find -name '*.ko' -exec cp -av {} $KERNELDIR/BUILT_I9300/lib/modules/ \;
+${CROSS_COMPILE}strip --strip-unneeded $KERNELDIR/BUILT_I9300/lib/modules/*
+cp $KERNELDIR/arch/arm/boot/zImage $KERNELDIR/BUILT_I9300/
+
+mv .git-halt .git
+
